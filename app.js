41b7507be67237405fbf8d6b55e6deefd1429490
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2023-03-02 21:41:32 -0300

    Criado model, controller e finalizando os métodos para o CRUD

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+const booksV1 = require("./routes/booksV1")
 var app = express();
 
 app.use(logger('dev'));
@@ -14,7 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/v1/livros", booksV1);
 
 module.exports = app;
---
+++ controllers/BooksController.js
@@ -0,0 +1,240 @@
+const { Book } = require("../models");
+
+const BooksController = {
+  index: async (req, res) => {
+    try {
+      const books = await Book.findAll();
+
+      if (!books) {
+        return res.status(404).json({ message: "Não há livros" });
+      }
+
+      return res.status(200).json({ data: books });
+    } catch (error) {
+      console.log(error);
+      if (error.name === "SequelizeConnectionRefusedError") {
+        return res.status(500).json({
+          error: true,
+          message: "Sistema indisponível, tente novamente mais tarde!",
+        });
+      }
+
+      if (error.name === "SequelizeUniqueConstraintError") {
+        return res.status(400).json(error.parent.sqlMessage);
+      }
+
+      if (error.name === "SequelizeValidationError") {
+        return res.status(400).json({
+          error: true,
+          message: `${error.errors[0].type} at ${error.errors[0].path}`,
+        });
+      }
+
+      return res.status(400).json({
+        error: true,
+        message: "Falha na requisição, tente novamente!",
+      });
+    }
+  },
+  show: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const book = await Book.findByPk(id);
+
+      if (!book) {
+        return res.status(404).json({ message: "Livro não encontrado" });
+      }
+
+      return res.status(200).json({ data: book });
+    } catch (error) {
+      console.log(error);
+      if (error.name === "SequelizeConnectionRefusedError") {
+        return res.status(500).json({
+          error: true,
+          message: "Sistema indisponível, tente novamente mais tarde!",
+        });
+      }
+
+      if (error.name === "SequelizeUniqueConstraintError") {
+        return res.status(400).json(error.parent.sqlMessage);
+      }
+
+      if (error.name === "SequelizeValidationError") {
+        return res.status(400).json({
+          error: true,
+          message: `${error.errors[0].type} at ${error.errors[0].path}`,
+        });
+      }
+
+      return res.status(400).json({
+        error: true,
+        message: "Falha na requisição, tente novamente!",
+      });
+    }
+  },
+  store: async (req, res) => {
+    try {
+      const { title, total_pages, author, release_year, stock } = req.body;
+
+      if (
+        title == "" ||
+        total_pages == "" ||
+        author == "" ||
+        release_year == "" ||
+        stock == ""
+      ) {
+        return res
+          .status(404)
+          .json({ message: "Verifique. Há campo(s) vazio(s)." });
+      }
+
+      const book = await Book.create({
+        title,
+        total_pages,
+        author,
+        release_year,
+        stock,
+      });
+
+      return res
+        .status(201)
+        .json({ message: "Livro criado com sucesso", data: book });
+
+    } catch (error) {
+      console.log(error);
+      if (error.name === "SequelizeConnectionRefusedError") {
+        return res.status(500).json({
+          error: true,
+          message: "Sistema indisponível, tente novamente mais tarde!",
+        });
+      }
+
+      if (error.name === "SequelizeUniqueConstraintError") {
+        return res.status(400).json(error.parent.sqlMessage);
+      }
+
+      if (error.name === "SequelizeValidationError") {
+        return res.status(400).json({
+          error: true,
+          message: `${error.errors[0].type} at ${error.errors[0].path}`,
+        });
+      }
+
+      return res.status(400).json({
+        error: true,
+        message: "Falha na requisição, tente novamente!",
+      });
+    }
+  },
+  edit: async (req, res) => {
+    try {
+      const {id} = req.params;
+
+      const { title, total_pages, author, release_year, stock } = req.body;
+
+      if ( title == "" || total_pages == "" || author == "" || release_year == "" || stock == "" ) {
+        return res
+          .status(404)
+          .json({ message: "Verifique. Há campo(s) vazio(s)." });
+      }
+
+      const existsBook = await Book.findByPk(id);
+
+      if (!existsBook) {
+        return res.status(404).json({ message: "Livro não encontrado" });
+      }
+
+      await Book.update({
+        title,
+        total_pages: Number(total_pages),
+        author,
+        release_year,
+        stock: Number(stock)
+      },{
+        where:{
+            id
+        }
+      });
+
+      const book = await Book.findByPk(id);
+
+      return res
+        .status(201)
+        .json({ message: "Livro atualizado com sucesso", data:book});
+        
+    } catch (error) {
+      console.log(error);
+      if (error.name === "SequelizeConnectionRefusedError") {
+        return res.status(500).json({
+          error: true,
+          message: "Sistema indisponível, tente novamente mais tarde!",
+        });
+      }
+
+      if (error.name === "SequelizeUniqueConstraintError") {
+        return res.status(400).json(error.parent.sqlMessage);
+      }
+
+      if (error.name === "SequelizeValidationError") {
+        return res.status(400).json({
+          error: true,
+          message: `${error.errors[0].type} at ${error.errors[0].path}`,
+        });
+      }
+
+      return res.status(400).json({
+        error: true,
+        message: "Falha na requisição, tente novamente!",
+      });
+    }
+  },
+  destroy: async (req, res) => {
+    try {
+      const {id} = req.params;
+
+      const existsBook = await Book.findByPk(id);
+
+      if (!existsBook) {
+        return res.status(404).json({ message: "Livro não encontrado" });
+      }
+
+      await Book.destroy({
+        where: {
+          id
+        },
+      })
+
+      return res
+        .status(201)
+        .json({ message: "Livro excluído com sucesso"});
+        
+    } catch (error) {
+      console.log(error);
+      if (error.name === "SequelizeConnectionRefusedError") {
+        return res.status(500).json({
+          error: true,
+          message: "Sistema indisponível, tente novamente mais tarde!",
+        });
+      }
+
+      if (error.name === "SequelizeUniqueConstraintError") {
+        return res.status(400).json(error.parent.sqlMessage);
+      }
+
+      if (error.name === "SequelizeValidationError") {
+        return res.status(400).json({
+          error: true,
+          message: `${error.errors[0].type} at ${error.errors[0].path}`,
+        });
+      }
+
+      return res.status(400).json({
+        error: true,
+        message: "Falha na requisição, tente novamente!",
+      });
+    }
+  },
+};
+
+module.exports = BooksController;
